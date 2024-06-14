247bbb418fcbf21e9be4e71219d29eaf3384a326
yuniruyuni <yuniruyuni@gmail.com>
2024-06-15 07:21:01 +0900

    docs: add comment for variable types

--- src/model/variable.ts
+++ src/model/variable.ts
@@ -1,13 +1,22 @@
 import { parse, EvalAstFactory } from 'jexpr';
 
+// Parameters is a recursive type that can be used to define a nested object structure.
+// Parameters contains expression string that starts with "$",
+// and it will be evaluated by the evaluate function into the Environment type.
 export type Parameters = {
   [key: string]: (Parameters | Array<Parameters> | string | number | boolean | undefined);
 };
 
+// Environment is a recursive type that can be used to define a nested object structure.
+// Environment contains the evaluated result of the Parameters type.
 export type Environment = {
   [key: string]: (Environment | Array<Environment> | string | number | boolean | undefined);
 };
 
+// evaluate function takes two arguments, target and envs.
+// target is the Parameters type that contains the expression string that starts with "$".
+// envs is the Environment type that contains the evaluated result of the Parameters type.
+// evaluate function returns the Environment type that contains the evaluated result of the target.
 export function evaluate<T extends Parameters>(target: T, envs: Environment): Environment {
   return Object.fromEntries(Object.entries(target).map(([key, val]) => {
     if(typeof val !== "string") return [key, val];
